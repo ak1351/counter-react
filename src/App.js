e5c37999302402ba2a1710d69bd151263ddfde4d
ak1351 <ajithraman822001@gmail.com>
2023-03-24 17:23:18 +0530

    React counter app first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const[ count,setCount]=useState(0);
+const[array,setarray]=useState([])
+
+  
+const num= array.filter((x)=> x%2===0)
+const num1= array.filter((x)=> x%2!==0)
+const num2= array.map((x) =>x*x  )
+// 
+console.log("hi ji",num2)
+
+function incre()
+  {
+  setCount(count+1);
+
+  setarray([...array,count])
+}
+function decre()
+  {
+  setCount(count-1);
+}
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={incre}> +</button>
+     <h3>  increment in{ count}</h3>
+       <button onClick={decre}> - </button>
+  <div style={{display:"flex",flexDiretion:"row",gap:'10px'}}>
+<div  >< h2> numbers</h2> {array.map((data)=><li> {data}</li>)}
+</div>
+<br></br>
+<div>
+<h2>  even</h2>{num.map((data)=><li> {data}</li>)} 
+</div>
+<br></br>
+<div>
+<h2>  odd</h2>{num1.map((data)=><li> {data}</li>)} 
+</div>
+<div>
+<h2>  sqrt</h2>{num2.map((data)=><li> {data}</li>)} 
+</div>
+</div>
     </div>
   );
 }
